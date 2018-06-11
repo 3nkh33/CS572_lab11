701f1eeb612f4d5d9bede74f85c45883d8b4dc46
Enkhee-lab <esambuuyondon@mum.edu>
2018-06-11 14:53:18 -0500

    lab11

---
+++ counter/src/app/counter.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+  <button (click)="decrease()">-</button>
+  {{counterValue}}
+  <button (click)="increase()">+</button>
+  `,
+  styles: []
+})
+export class CounterComponent implements OnInit {
+  private counterValue = 0;
+
+  @Input('value') counter: number;
+  @Output() counterChange = new EventEmitter();
+  constructor() { }
+
+  increase() {
+    this.counterValue++;
+    this.Counter = this.counterValue;
+    this.counterChange.emit(this.counterValue);
+    return false;
+  }
+  decrease() {
+    this.counterValue--;
+    this.Counter = this.counterValue;
+    this.counterChange.emit(this.counterValue);
+    return false;
+  }
+  ngOnInit() {
+    this.counterValue = this.counter ? this.counter : 0;
+  }
+  set Counter(value: number) {
+    this.counter = value;
+  }
+}
